19f54622f55dcbf32307b6826c4f2a7c61cb6978
VodeniZeko <salexy.es@gmail.com>
2020-04-08 00:49:25 -0700

    first push

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>hi from SearchScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    alignSelf: "center"
+  }
+});
+export default SearchScreen;
